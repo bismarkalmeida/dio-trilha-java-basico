4d91481d29b153130f1e067787e8b8d86c832c3e
Bismark Santos <bismark.almeida@hotmail.com>
2024-06-18 23:19:38 -0300

    Impletar o desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá mundo!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digito o numero da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o numero da agencia");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o seu saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + " obrigado por criar um conta em nosso banco, sua agência " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível");
     }
 }
